894bbf7f05ff9460f8979c87eb0c0d4597b4acef
unknown <jeremyacosta2019@gmail.com>
2018-07-12 12:02:12 -0400

    notsorandom work

---
+++ notsoRandom.js
@@ -0,0 +1,9 @@
+function number(parameter){
+console.log(Math.floor(Math.random()*parameter))
+} 
+number(333) 
+
+function randomLetter(stringEx){
+    console.log(stringEx.charAt(Math.random()*stringEx.length))
+} 
+randomLetter("1234")git
